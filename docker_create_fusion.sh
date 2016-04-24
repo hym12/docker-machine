52c4e4a5c033def62d61bbee17e82a68f3826b1f
paoloid <pt@ideato.info>
2016-04-24 13:41:48 +0200

    Changed VMware Fusion provisioning

--- docker_create_fusion.sh
+++ docker_create_fusion.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 docker-machine create -d vmwarefusion \
- --generic-ssh-user vagrant \
- --generic-ssh-key ~/.vagrant.d/insecure_private_key \
- --generic-ip-address 10.0.0.10 \
+ --vmwarefusion-cpu-count 2 \
+ --vmwarefusion-memory-size 4096 \
+ --vmwarefusion-disk-size 40000 \
  default
+
+sleep 5
+mkdir -p ~/bin
+cp docker_box_fusion.sh ~/bin
+chmod 750 ~/bin/docker_box_fusion.sh
+sudo nfsd enable
+
+sleep 5
+cat << EOF >> ~/.profile
+DOCKER_HELPERS_OSX_MACHINE="default"
+DOCKER_HELPERS_OSX_NFS_EXPORT=$(pwd)
+source ~/bin/docker_box_fusion.sh
+EOF
+
+source ~/.profile
+eval $(docker-machine env default)
+dm restart
